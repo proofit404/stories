f4470245cdeaef468aeb3f79c489ee36c3b3531d
Artem Malyshev <proofit404@gmail.com>
2018-04-02 17:07:06 +0300

    Create PyPI package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="stories",
+    version="0.0.1",
+    description="Define a user story in the business transaction DSL",
+    url="https://github.com/proofit404/stories",
+    license="BSD",
+    author="Artem Malyshev",
+    author_email="proofit404@gmail.com",
+    py_modules=["stories"],
+)
