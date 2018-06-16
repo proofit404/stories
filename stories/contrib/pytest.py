03ece364995d18e3f725305c39ce1f98b2c28a32
Artem Malyshev <proofit404@gmail.com>
2018-06-16 06:44:19 +0300

    Don't track proxy creation in the pytest plugin.

--- stories/contrib/pytest.py
+++ stories/contrib/pytest.py
@@ -5,52 +5,31 @@ import stories._proxy
 from _pytest.config import hookimpl
 
 
-origin_make_proxy = stories._proxy.make_proxy
 origin_context_init = stories._context.Context.__init__
 
 
-def track_proxy(storage):
-    def wrapper(obj, ctx, history):
-
-        proxy = origin_make_proxy(obj, ctx, history)
-        storage[ctx.__position__][0] = proxy
-        return proxy
-
-    return wrapper
-
-
-def track_context(storage, sequence):
-    def wrapper(ctx, ns):
-
-        origin_context_init(ctx, ns)
-        position = next(sequence)
-        ctx.__position__ = position
-        storage[position] = [None, ctx]
+def track_context(storage):
+    def wrapper(ctx, ns, history=None):
+        origin_context_init(ctx, ns, history=history)
+        storage.append(ctx)
 
     return wrapper
 
 
 @hookimpl(hookwrapper=True)
 def pytest_runtest_call(item):
-    sequence = itertools.count()
-    storage = {}
-    stories._run.make_proxy = track_proxy(storage)
-    stories._context.Context.__init__ = track_context(storage, sequence)
+    storage = []
+    stories._context.Context.__init__ = track_context(storage)
     yield
-    stories._run.make_proxy = origin_make_proxy
     stories._context.Context.__init__ = origin_context_init
     item.add_report_section(
         "call",
         "stories",
         "\n\n".join(
-            map(
-                repr,
-                filter(
-                    None,
-                    itertools.chain.from_iterable(
-                        storage[i] for i in range(len(storage))
-                    ),
-                ),
+            itertools.chain.from_iterable(
+                zip(
+                    map(lambda ctx: "\n".join(ctx.history), storage), map(repr, storage)
+                )
             )
         ),
     )
