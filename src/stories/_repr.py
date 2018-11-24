e13614a3cfbb73ef0a925e4659beea1cd68cc560
Artem Malyshev <proofit404@gmail.com>
2018-11-25 02:30:57 +0300

    Fix story class attribute representation for classes with substory DI.

--- src/stories/_repr.py
+++ src/stories/_repr.py
@@ -21,7 +21,7 @@ def story_representation(is_story, first_line, cls, obj, collected):
     result = [first_line]
     if collected:
         for name in collected:
-            attr = getattr(obj, name)
+            attr = getattr(obj or cls, name, None)
             if is_story(attr):
                 if attr.cls is cls:
                     first = name
@@ -32,7 +32,8 @@ def story_representation(is_story, first_line, cls, obj, collected):
                 )
                 result.extend(["  " + line for line in sub_result.split("\n")])
             else:
-                result.append("  " + name)
+                suffix = " ??" if attr is None else ""
+                result.append("  " + name + suffix)
     else:
         result.append("  <empty>")
 
--- tests/test_story.py
+++ tests/test_story.py
@@ -78,3 +78,53 @@ SubstoryDI.y
   after
 """.strip()
     assert story == expected
+
+
+def test_story_class_attribute_representation():
+
+    story = repr(examples.Empty.x)
+    expected = """
+Empty.x
+  <empty>
+""".strip()
+    assert story == expected
+
+    story = repr(examples.EmptySubstory.y)
+    expected = """
+EmptySubstory.y
+  x
+    <empty>
+""".strip()
+    assert story == expected
+
+    story = repr(examples.Simple.x)
+    expected = """
+Simple.x
+  one
+  two
+  three
+""".strip()
+    assert story == expected
+
+    story = repr(examples.SimpleSubstory.y)
+    expected = """
+SimpleSubstory.y
+  start
+  before
+  x
+    one
+    two
+    three
+  after
+""".strip()
+    assert story == expected
+
+    story = repr(examples.SubstoryDI.y)
+    expected = """
+SubstoryDI.y
+  start
+  before
+  x ??
+  after
+""".strip()
+    assert story == expected
