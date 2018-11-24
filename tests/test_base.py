2c5c6cc460ca524bd634e712a90422a509dd850d
Artem Malyshev <proofit404@gmail.com>
2018-11-25 02:10:06 +0300

    Move return test cases in to separate module.

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,5 @@
 import examples
 import pytest
-from stories import Failure, Result, Skip, Success
 from stories.exceptions import FailureError
 
 
@@ -348,44 +347,3 @@ SubstoryDI.y
   after
 """.strip()
     assert story == expected
-
-
-def test_result_representation():
-
-    result = Result(1)
-    assert repr(result) == "Result(1)"
-
-
-def test_success_representation():
-
-    success = Success(foo="bar", baz=2)
-    assert repr(success) in {"Success(foo='bar', baz=2)", "Success(baz=2, foo='bar')"}
-
-
-def test_failure_representation():
-
-    failure = Failure()
-    assert repr(failure) == "Failure()"
-
-    failure = Failure("test")
-    assert repr(failure) == "Failure('test')"
-
-
-def test_skip_representation():
-
-    skip = Skip()
-    assert repr(skip) == "Skip()"
-
-
-def test_failure_summary_representation():
-
-    expected = "Failure()"
-    result = examples.Simple().x.run(2, 2)
-    assert repr(result) == expected
-
-
-def test_success_summary_representation():
-
-    expected = "Success()"
-    result = examples.Simple().x.run(1, 3)
-    assert repr(result) == expected
---
+++ tests/test_return.py
@@ -0,0 +1,43 @@
+import examples
+from stories import Failure, Result, Skip, Success
+
+
+def test_result_representation():
+
+    result = Result(1)
+    assert repr(result) == "Result(1)"
+
+
+def test_success_representation():
+
+    success = Success(foo="bar", baz=2)
+    assert repr(success) in {"Success(foo='bar', baz=2)", "Success(baz=2, foo='bar')"}
+
+
+def test_failure_representation():
+
+    failure = Failure()
+    assert repr(failure) == "Failure()"
+
+    failure = Failure("test")
+    assert repr(failure) == "Failure('test')"
+
+
+def test_skip_representation():
+
+    skip = Skip()
+    assert repr(skip) == "Skip()"
+
+
+def test_failure_summary_representation():
+
+    expected = "Failure()"
+    result = examples.Simple().x.run(2, 2)
+    assert repr(result) == expected
+
+
+def test_success_summary_representation():
+
+    expected = "Success()"
+    result = examples.Simple().x.run(1, 3)
+    assert repr(result) == expected
