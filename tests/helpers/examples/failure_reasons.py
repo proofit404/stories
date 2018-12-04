bfcc464c626b65c5920c3082f06e696eec2a8942
Artem Malyshev <proofit404@gmail.com>
2018-12-04 08:42:19 +0300

    Verify failure protocol with substories in the same class.

--- tests/helpers/examples/failure_reasons.py
+++ tests/helpers/examples/failure_reasons.py
@@ -3,12 +3,22 @@
 # [ ]: Check incoming reasons type in the StoryFactory.
 #
 # [ ]: Story and substory protocol mismatch.
-from enum import Enum, unique
+from enum import Enum
 
-from stories import Failure, StoryFactory
+from stories import Failure, StoryFactory, Success
 
 
-class Common(object):
+story_with_list = StoryFactory(failures=["foo", "bar", "baz"])
+
+
+Errors = Enum("Errors", "foo bar baz")
+story_with_enum = StoryFactory(failures=Errors)
+
+
+# Simple story.
+
+
+class CommonSimple(object):
 
     # Wrong reason.
 
@@ -21,51 +31,86 @@ class Common(object):
         return Failure()
 
 
-# Simple.
-
-
-story = StoryFactory(failures=["foo", "bar", "baz"])
-
-
-class SimpleWithList(Common):
-    @story
+class SimpleWithList(CommonSimple):
+    @story_with_list
     def x(I):
         I.one
+
+    @story_with_list
+    def y(I):
+        I.two
+
+    @story_with_list
+    def z(I):
+        I.three
 
     def one(self, ctx):
         return Failure("foo")
 
-    @story
+
+class SimpleWithEnum(CommonSimple):
+    @story_with_enum
+    def x(I):
+        I.one
+
+    @story_with_enum
     def y(I):
         I.two
 
-    @story
+    @story_with_enum
     def z(I):
         I.three
-
-
-@unique
-class Errors(Enum):
-    foo = 1
-    bar = 2
-    baz = 3
-
-
-story = StoryFactory(failures=Errors)
-
-
-class SimpleWithEnum(Common):
-    @story
-    def x(I):
-        I.one
 
     def one(self, ctx):
         return Failure(Errors.foo)
 
-    @story
-    def y(I):
-        I.two
 
-    @story
-    def z(I):
-        I.three
+# Substory in the same class.
+
+
+class CommonSubstory(object):
+    def before(self, ctx):
+        return Success()
+
+    def after(self, ctx):
+        return Success()
+
+
+class SimpleSubstoryWithList(CommonSubstory, SimpleWithList):
+    @story_with_list
+    def a(I):
+        I.before
+        I.x
+        I.after
+
+    @story_with_list
+    def b(I):
+        I.before
+        I.y
+        I.after
+
+    @story_with_list
+    def c(I):
+        I.before
+        I.z
+        I.after
+
+
+class SimpleSubstoryWithEnum(CommonSubstory, SimpleWithEnum):
+    @story_with_enum
+    def a(I):
+        I.before
+        I.x
+        I.after
+
+    @story_with_enum
+    def b(I):
+        I.before
+        I.y
+        I.after
+
+    @story_with_enum
+    def c(I):
+        I.before
+        I.z
+        I.after
