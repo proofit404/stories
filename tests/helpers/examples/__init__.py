23096cc98bfeb4e13d409db12f9c1c899b4423b9
Artem Malyshev <proofit404@gmail.com>
2019-03-02 07:01:50 +0300

    Handle uninstalled libraries in environments like flake8 and isort.

--- tests/helpers/examples/__init__.py
+++ tests/helpers/examples/__init__.py
@@ -1,23 +1,37 @@
 # flake8: noqa
-import examples.context_contract_cerberus
-import examples.context_contract_marshmallow
-import examples.context_contract_raw
 import examples.contract
 import examples.failure_reasons
 import examples.methods
 import examples.shortcuts
 
 
-contract_modules = [
-    examples.context_contract_marshmallow,
-    examples.context_contract_cerberus,
-    examples.context_contract_raw,
-]
+contract_modules = []
 
 
 try:
     import examples.context_contract_pydantic
 
-    contract_modules.insert(0, examples.context_contract_pydantic)
-except SyntaxError:
+    contract_modules.append(examples.context_contract_pydantic)
+except (SyntaxError, ImportError):
+    pass
+
+try:
+    import examples.context_contract_marshmallow
+
+    contract_modules.append(examples.context_contract_marshmallow)
+except ImportError:
+    pass
+
+try:
+    import examples.context_contract_cerberus
+
+    contract_modules.append(examples.context_contract_cerberus)
+except ImportError:
+    pass
+
+try:
+    import examples.context_contract_raw
+
+    contract_modules.append(examples.context_contract_raw)
+except ImportError:
     pass
