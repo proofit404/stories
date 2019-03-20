dfd2d613aa9b1d527e46f6300fb7b66492002cb8
Artem Malyshev <proofit404@gmail.com>
2019-03-20 03:13:08 +0300

    Ignore unused variable warning in the compatibility module.

--- src/stories/_compat.py
+++ src/stories/_compat.py
@@ -45,4 +45,4 @@ try:
     from prettyprinter import pformat
 except ImportError:
     # Prettyprinter package is not installed.
-    from pprint import pformat
+    from pprint import pformat  # noqa
