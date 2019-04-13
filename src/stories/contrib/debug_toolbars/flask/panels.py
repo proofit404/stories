c87fc109576ad6cb8d55f43848b0947376de0f48
Max Hollmann <maxhollmann@gmail.com>
2019-04-13 14:02:36 +0200

    Flask debug-toolbar contrib

---
+++ src/stories/contrib/debug_toolbars/flask/panels.py
@@ -0,0 +1,68 @@
+"""
+stories.contrib.debug_toolbars.flask.panels
+-------------------------------------------
+
+This module contains integration with flask-debugtoolbar.
+
+:copyright: (c) 2018-2019 dry-python team.
+:license: BSD, see LICENSE for more details.
+"""
+
+import stories._context
+from flask_debugtoolbar.panels import DebugPanel
+from flask import render_template
+
+
+original_context_init = stories._context.Context.__init__
+
+
+def track_context(storage):
+    def wrapper(ctx, *args, **kwargs):
+        original_context_init(ctx, *args, **kwargs)
+        storage.append(ctx)
+
+    return wrapper
+
+
+def pluralize(number, singular, plural=None):
+    if plural is None:
+        plural = singular + "s"
+    if number == 1:
+        return "%d %s" % (number, singular)
+    else:
+        return "%d %s" % (number, plural)
+
+
+class StoriesPanel(DebugPanel):
+    name = "Stories"
+    has_content = True
+
+    def __init__(self, *args, **kwargs):
+        DebugPanel.__init__(self, *args, **kwargs)
+        self.storage = []
+        self.enable_instrumentation()
+
+    def nav_title(self):
+        return "Stories"
+
+    def nav_subtitle(self):
+        count = len(self.storage)
+        return pluralize(count, "call")
+
+    def title(self):
+        count = len(self.storage)
+        return "Context and execution path of %s" % pluralize(count, 'story', 'stories')
+
+    def url(self):
+        return '#'
+
+    def content(self):
+        return render_template(
+            "stories/debug_toolbar/stories_panel.html",
+            stories=self.storage)
+
+    def enable_instrumentation(self):
+        stories._context.Context.__init__ = track_context(self.storage)
+
+    def disable_instrumentation(self):
+        stories._context.Context.__init__ = original_context_init
