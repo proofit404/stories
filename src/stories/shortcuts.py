d132074c56761c254187eb29adb0905f72db45f7
Artem Malyshev <proofit404@gmail.com>
2019-07-03 01:45:03 +0300

    Annotate shortcuts module.

--- src/stories/shortcuts.py
+++ src/stories/shortcuts.py
@@ -8,11 +8,19 @@ This module contains convenient functions to reduce boilerplate code.
 :license: BSD, see LICENSE for more details.
 """
 
+from typing import Callable, Type, Union
+
 from ._mounted import ClassMountedStory
+from ._types import ClassWithSpec, ContextContract, FailureProtocol
 
 
-def contract_in(cls, *args):
+def contract_in(
+    cls,  # type: Type[ClassWithSpec]
+    *args  # type: ContextContract
+):
+    # type: (...) -> Union[ContextContract, Callable[[ContextContract], ContextContract]]
     def setter(contract):
+        # type: (ContextContract) -> ContextContract
         for attrname in dir(cls):
             attribute = getattr(cls, attrname)
             if type(attribute) is ClassMountedStory:
@@ -25,8 +33,13 @@ def contract_in(cls, *args):
         return setter
 
 
-def failures_in(cls, *args):
+def failures_in(
+    cls,  # type: Type[ClassWithSpec]
+    *args  # type: FailureProtocol
+):
+    # type: (...) -> Union[FailureProtocol, Callable[[FailureProtocol], FailureProtocol]]
     def setter(failures):
+        # type: (FailureProtocol) -> FailureProtocol
         for attrname in dir(cls):
             attribute = getattr(cls, attrname)
             if type(attribute) is ClassMountedStory:
