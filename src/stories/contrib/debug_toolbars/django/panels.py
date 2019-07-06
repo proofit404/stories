f63fa70da24205e1029432609040a4c31341d681
Artem Malyshev <proofit404@gmail.com>
2019-07-06 03:18:47 +0300

    Annotate Django Debug Toolbar module with types.

--- src/stories/contrib/debug_toolbars/django/panels.py
+++ src/stories/contrib/debug_toolbars/django/panels.py
@@ -8,17 +8,29 @@ This module contains integration with Django Debug Toolbar.
 :license: BSD, see LICENSE for more details.
 """
 
-import stories._context
+from typing import Callable, List, Union
+
 from debug_toolbar.panels import Panel
+from debug_toolbar.toolbar import DebugToolbar
+from django.http import HttpRequest, HttpResponse
 from django.utils.translation import ugettext_lazy as _
 from django.utils.translation import ungettext_lazy as __
+
+import stories._context
+from stories._types import AbstractContext
 
 
 origin_context_init = stories._context.Context.__init__
 
 
+GetResponse = Callable[[HttpRequest], HttpResponse]
+PanelArgument = Union[DebugToolbar, GetResponse]
+
+
 def track_context(storage):
+    # type: (List[AbstractContext]) -> Callable[[AbstractContext], None]
     def wrapper(ctx):
+        # type: (AbstractContext) -> None
         origin_context_init(ctx)
         storage.append(ctx)
 
@@ -35,11 +47,13 @@ class StoriesPanel(Panel):
 
     @property
     def nav_subtitle(self):
+        # type: () -> str
         count = len(self.storage)
         return __("%(count)d call", "%(count)d calls", count) % {"count": count}
 
     @property
     def title(self):
+        # type: () -> str
         count = len(self.storage)
         return __(
             "Context and execution path of %(count)d story",
@@ -50,14 +64,18 @@ class StoriesPanel(Panel):
     # Implement the Collector.
 
     def __init__(self, *args, **kwargs):
+        # type: (*PanelArgument, **PanelArgument) -> None
         super(StoriesPanel, self).__init__(*args, **kwargs)
-        self.storage = []
+        self.storage = []  # type: List[AbstractContext]
 
     def enable_instrumentation(self):
+        # type: () -> None
         stories._context.Context.__init__ = track_context(self.storage)
 
     def disable_instrumentation(self):
+        # type: () -> None
         stories._context.Context.__init__ = origin_context_init
 
     def generate_stats(self, request, response):
+        # type: (HttpRequest, HttpResponse) -> None
         self.record_stats({"stories": self.storage})
