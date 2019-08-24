f18f07583a4da5ff0ff2a62669086f6a4ab49d79
Artem Malyshev <proofit404@gmail.com>
2019-08-24 22:12:37 +0300

    Load django database fixtures before doctest.

--- tests/helpers/mddoctest.py
+++ tests/helpers/mddoctest.py
@@ -4,10 +4,13 @@ from glob import glob
 
 from django.apps import apps
 from django.conf import settings
+from django.core import management
 
 
 def main():
     apps.populate(settings.INSTALLED_APPS)
+    management.call_command("migrate")
+    management.call_command("loaddata", "examples.yaml")
     markdown_files = glob("**/*.md", recursive=True)
     exit_code = 0
     for markdown_file in markdown_files:
