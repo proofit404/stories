acb3d34085ae150b2cdf3e4c578230d0609892ec
Artem Malyshev <proofit404@gmail.com>
2019-10-11 00:56:22 +0300

    Apply black to all pyi files.

--- src/_stories/contract.pyi
+++ src/_stories/contract.pyi
@@ -1,15 +1,13 @@
-from typing import (
-    Any,
-    Callable,
-    Dict,
-    List,
-    Optional,
-    Set,
-    Tuple,
-    Type,
-    Union,
-    overload,
-)
+from typing import Any
+from typing import Callable
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import overload
+from typing import Set
+from typing import Tuple
+from typing import Type
+from typing import Union
 
 from cerberus.validator import Validator
 from marshmallow.schema import Schema
@@ -19,79 +17,55 @@ from pydantic.main import BaseModel
 from typing_extensions import Literal
 
 from _stories.context import Context
-from _stories.failures import NotNullExecProtocol, NullExecProtocol
-from _stories.marker import BeginningOfStory, EndOfStory
-from _stories.returned import Failure, Result, Skip, Success
-
+from _stories.failures import NotNullExecProtocol
+from _stories.failures import NullExecProtocol
+from _stories.marker import BeginningOfStory
+from _stories.marker import EndOfStory
+from _stories.returned import Failure
+from _stories.returned import Result
+from _stories.returned import Skip
+from _stories.returned import Success
 
 class PydanticValidator:
     def __init__(self, spec: BaseModel, field: Field) -> None: ...
-
     def __call__(self, value: Any) -> Any: ...
-
     def __repr__(self) -> str: ...
-
 
 class MarshmallowValidator:
     def __init__(self, spec: Schema, field: str) -> None: ...
-
     def __call__(self, value: Any) -> Any: ...
-
     def __repr__(self) -> str: ...
-
 
 class CerberusValidator:
     def __init__(self, spec: Validator, field: str) -> None: ...
-
     def __call__(self, value: Any) -> Any: ...
-
     def __repr__(self) -> str: ...
-
 
 class RawValidator:
     def __init__(self, validator: Callable) -> None: ...
-
     def __call__(self, value: Any) -> Any: ...
-
     def __repr__(self) -> str: ...
 
-
 def disassemble_pydantic(spec: BaseModel) -> Dict[str, PydanticValidator]: ...
-
-
 def disassemble_marshmallow(spec: Schema) -> Dict[str, MarshmallowValidator]: ...
-
-
 def disassemble_cerberus(spec: Validator) -> Dict[str, CerberusValidator]: ...
-
-
 def disassemble_raw(spec: Dict[str, Callable]) -> Dict[str, RawValidator]: ...
-
-
 @overload
 def make_contract(
     cls_name: str, name: str, arguments: List[str], spec: Literal[None]
 ) -> NullContract: ...
-
-
 @overload
 def make_contract(
     cls_name: str, name: str, arguments: List[str], spec: Type[BaseModel]
 ) -> SpecContract: ...
-
-
 @overload
 def make_contract(
     cls_name: str, name: str, arguments: List[str], spec: Type[Schema]
 ) -> SpecContract: ...
-
-
 @overload
 def make_contract(
     cls_name: str, name: str, arguments: List[str], spec: Dict[str, Callable]
 ) -> SpecContract: ...
-
-
 def check_arguments_definitions(
     cls_name: str,
     name: str,
@@ -102,28 +76,19 @@ def check_arguments_definitions(
     ],
 ) -> None: ...
 
-
 class NullContract:
     def __init__(self, cls_name: str, name: str, arguments: List[str]) -> None: ...
-
     def set_null(self) -> None: ...
-
     def make_argset(self) -> None: ...
-
     def check_story_call(self, kwargs: Dict[str, Any]) -> Dict[str, Any]: ...
-
     def check_substory_call(self, ctx: Context) -> None: ...
-
     def check_success_statement(
         self, method: Callable, ctx: Context, ns: Dict[str, Any]
     ) -> Dict[str, Any]: ...
-
     def __repr__(self) -> str: ...
-
     def format_contract_fields(
         self, *fieldset: Tuple[Dict[str, Set[Tuple[None, str, str]]], ...]
     ) -> str: ...
-
 
 class SpecContract(NullContract):  # FIXME: Generic.
     def __init__(
@@ -139,28 +104,20 @@ class SpecContract(NullContract):  # FIXME: Generic.
         ],
         origin: Any,
     ) -> None: ...
-
     def set_null(self) -> None: ...
-
     def make_argset(self) -> None: ...
-
     def make_declared(self) -> None: ...
-
     def check_story_call(
         self, kwargs: Dict[str, Union[Dict[str, str], str, int, List[int], List[str]]]
     ) -> Dict[str, Union[int, List[int], Dict[str, str], Dict[str, int]]]: ...
-
     def check_success_statement(
         self,
         method: Callable[[Context], Union[Result, Success, Failure, Skip]],
         ctx: Context,
         ns: Dict[str, Any],
     ) -> Dict[str, Any]: ...
-
     def identify(self, ns: Dict[str, Any]) -> Set[str]: ...
-
     def validate(self, ns: Dict[str, Any]) -> Any: ...
-
     def validate_spec(
         self,
         result: Dict[str, Union[Dict[str, str], int]],
@@ -169,7 +126,6 @@ class SpecContract(NullContract):  # FIXME: Generic.
         key: str,
         value: Union[List[str], Dict[str, str], str],
     ) -> None: ...
-
     def validate_argset(
         self,
         result: Dict[str, Union[Dict[str, str], str, int, List[int]]],
@@ -183,7 +139,6 @@ class SpecContract(NullContract):  # FIXME: Generic.
         key: str,
         value: Union[List[int], List[str], Dict[str, str], str],
     ) -> None: ...
-
     def assign_result(
         self,
         result: Dict[str, Union[Dict[str, str], str, int, List[int]]],
@@ -196,35 +151,22 @@ class SpecContract(NullContract):  # FIXME: Generic.
         value: Union[List[int], str, Dict[str, str], List[str]],
         new_value: Any,
     ) -> None: ...
-
     def __repr__(self) -> str: ...
-
     def format_contract_fields(
         self, *fieldset: Tuple[Dict[str, Set[Tuple[None, str, str]]], ...]
     ) -> str: ...
 
-
 def format_violations(ns: Dict[str, Any], errors: Dict[str, Any]) -> str: ...
-
-
 def combine_contract(
     parent: Union[SpecContract, NullContract], child: Union[SpecContract, NullContract]
 ) -> None: ...
-
-
 def combine_argsets(
     parent: Union[SpecContract, NullContract], child: Union[SpecContract, NullContract]
 ) -> None: ...
-
-
 def combine_declared(parent: SpecContract, child: SpecContract) -> None: ...
-
-
 def format_contract(
     contract: Union[SpecContract, NullContract]
 ) -> Optional[Union[Type[BaseModel], Type[Schema], Type[dict], Type[Validator]]]: ...
-
-
 def maybe_extend_downstream_argsets(
     methods: List[
         Tuple[
