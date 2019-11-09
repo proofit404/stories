8f046529da00aa8ec8a1be4109bc5a45f52a2e6e
Artem Malyshev <proofit404@gmail.com>
2019-11-09 15:12:33 +0300

    Split configs. (#259)

--- tests/helpers/mddoctest.py
+++ tests/helpers/mddoctest.py
@@ -1,6 +1,7 @@
 import sys
 from doctest import testfile
 from glob import glob
+from textwrap import dedent
 from types import ModuleType
 
 from django.apps import apps
@@ -8,27 +9,32 @@ from django.conf import settings
 from django.core import management
 
 
-pdb = ModuleType("pdb")
-pdb.pm = lambda: print(  # FIXME: Run real pdb.
-    """
-> example.py(73)apply_discount()
--> return price * self.fraction
-(Pdb) ll
- 71      def apply_discount(self, price):
- 72
- 73  ->      return price * self.fraction
-(Pdb) args
-self = <example.PromoCode>
-price = None
-    """.strip()
-)
+def setup():
+    management.call_command("migrate")
+    management.call_command("loaddata", "examples.yaml")
+    # FIXME: Run real pdb.
+    pdb = ModuleType("pdb")
+    pdb.pm = lambda: print(
+        dedent(
+            """
+            > example.py(73)apply_discount()
+            -> return price * self.fraction
+            (Pdb) ll
+             71      def apply_discount(self, price):
+             72
+             73  ->      return price * self.fraction
+            (Pdb) args
+            self = <example.PromoCode>
+            price = None
+            """
+        ).strip()
+    )
+    sys.modules["pdb"] = pdb
 
 
 def main():
     apps.populate(settings.INSTALLED_APPS)
-    management.call_command("migrate")
-    management.call_command("loaddata", "examples.yaml")
-    sys.modules["pdb"] = pdb
+    setup()
     markdown_files = glob("**/*.md", recursive=True)
     exit_code = 0
     for markdown_file in markdown_files:
