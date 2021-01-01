a0f61225c58f2d4cca0088000c444bd6ef203b68
Artem Malyshev <proofit404@gmail.com>
2021-01-01 20:03:48 +0300

    chore: happy new year #574

--- src/stories/contrib/debug_toolbars/django.py
+++ src/stories/contrib/debug_toolbars/django.py
@@ -1,12 +1,4 @@
-"""
-stories.contrib.debug_toolbars.django
--------------------------------------
-
-This module contains integration with Django Debug Toolbar.
-
-:copyright: (c) 2018-2020 Artem Malyshev.
-:license: BSD, see LICENSE for more details.
-"""
+"""This module contains integration with Django Debug Toolbar."""
 from _stories.contrib.debug_toolbars.django.panels import StoriesPanel
 
 
--- src/stories/contrib/pytest.py
+++ src/stories/contrib/pytest.py
@@ -1,12 +1,4 @@
-"""
-stories.contrib.pytest
-----------------------
-
-This module contains integration with PyTest framework.
-
-:copyright: (c) 2018-2020 Artem Malyshev.
-:license: BSD, see LICENSE for more details.
-"""
+"""This module contains integration with PyTest framework."""
 from _stories.contrib.pytest import pytest_runtest_call
 
 
