5f2f0b75a066071c862d3e0abccdd6d3393c9401
Artem Malyshev <proofit404@gmail.com>
2021-04-01 01:56:45 +0300

    docs: rewrite README example in new syntax #630

---
+++ tests/helpers/app/repositories.py
@@ -0,0 +1,22 @@
+from app.entities import Category
+from app.entities import Profile
+from app.entities import Subscription
+
+
+def load_category(category_id):
+    """Load category from db."""
+    names = {1: "Books", 2: "Movies"}
+    costs = {1: 7, 2: 17}
+    return Category(name=names[category_id], cost=costs[category_id])
+
+
+def load_profile(profile_id):
+    """Load profile from db."""
+    balances = {1: 8, 2: 8}
+    return Profile(balance=balances[profile_id])
+
+
+def create_subscription(profile_id, category_id):
+    """Save Subscription into db."""
+    durations = {(1, 1): "30 days"}
+    return Subscription(duration=durations[(profile_id, category_id)])
