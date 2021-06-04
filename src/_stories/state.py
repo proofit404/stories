38d3d335cce6012f3672a0f49850b71e1e005f91
Artem Malyshev <proofit404@gmail.com>
2021-06-04 03:32:41 +0300

    feat: execute steps in given order #664

--- src/_stories/state.py
+++ src/_stories/state.py
@@ -2,4 +2,5 @@ class State:
     """Business process state."""
 
     def __init__(self, **arguments):
-        ...
+        for argument, value in arguments.items():
+            object.__setattr__(self, argument, value)
