841d37af25b908d17acff5abca95f99d01bb9e2a
Artem Malyshev <proofit404@gmail.com>
2021-08-29 02:05:53 +0300

    feat: implement @initiate restrictions #649

--- tests/test_initiate.py
+++ tests/test_initiate.py
@@ -1,8 +1,11 @@
 """Tests related to @initiate decorator."""
+import pytest
+
 from stories import I
 from stories import initiate
 from stories import State
 from stories import Story
+from stories.exceptions import StoryError
 
 
 def test_initiate(r, m):
@@ -86,3 +89,60 @@ def test_initiate(r, m):
         "a3s3",
         "a4s1",
     ]
+
+
+def test_deny_functions():
+    """Deny to decorate functions by @initiate decorator."""
+    with pytest.raises(StoryError) as exc_info:
+
+        @initiate
+        def a1():
+            raise RuntimeError
+
+    assert str(exc_info.value) == "@initiate can decorate Story subclasses only"
+
+
+def test_deny_non_story_classes():
+    """Deny to decorate regular classes by @initiate decorator."""
+    with pytest.raises(StoryError) as exc_info:
+
+        @initiate
+        class A1:
+            pass
+
+    assert str(exc_info.value) == "@initiate can decorate Story subclasses only"
+
+
+def test_deny_step_definitions(m):
+    """Deny to define steps on story decorated by @initiate decorator."""
+    with pytest.raises(StoryError) as exc_info:
+
+        @initiate
+        class A1(Story):
+            I.a1s1
+            I.a1s2
+            I.a1s3
+
+            a1s1 = m._normal_method
+
+    expected = "Story decorated by @initiate can not have step methods"
+
+    assert str(exc_info.value) == expected
+
+
+def test_deny_constructor_definition():
+    """Deny to define constructor method on story decorated by @initiate decorator."""
+    with pytest.raises(StoryError) as exc_info:
+
+        @initiate
+        class A1(Story):
+            I.a1s1
+            I.a1s2
+            I.a1s3
+
+            def __init__(self):
+                raise RuntimeError
+
+    expected = "Story decorated by @initiate can not have constructor defined"
+
+    assert str(exc_info.value) == expected
