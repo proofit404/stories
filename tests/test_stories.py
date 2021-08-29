abc34a936b7087dcbb2d909e31d34aba46fe04cf
Artem Malyshev <proofit404@gmail.com>
2021-08-29 09:47:44 +0300

    feat: propagate exceptions #631

--- tests/test_stories.py
+++ tests/test_stories.py
@@ -1,4 +1,6 @@
 """Tests related to stories module."""
+import pytest
+
 from stories import I
 from stories import State
 from stories import Story
@@ -111,3 +113,65 @@ def test_execute_steps(r, m):
     state = State(calls=[])
     r.run(story, state)
     assert state.calls == ["c1s1", "b1s1", "a1s1", "a1s2", "a1s3", "b1s2", "c1s2"]
+
+
+def test_propagate_exceptions(r, m):
+    """Exceptions raised by story steps should be propagated to the caller."""
+
+    class A1(Story):
+        I.a1s1
+        I.a1s2
+        I.a1s3
+
+        a1s1 = m._normal_method
+        a1s2 = m._normal_method
+        a1s3 = m._error_method("error in a1s3")
+
+    class B1(Story):
+        I.b1s1
+        I.a1
+        I.b1s2
+
+        b1s1 = m._error_method("error in b1s1")
+        b1s2 = m._normal_method
+
+        def __init__(self):
+            self.a1 = A1()
+
+    class C1(Story):
+        I.c1s1
+        I.b1
+        I.c1s2
+
+        c1s1 = m._error_method("error in c1s1")
+        c1s2 = m._normal_method
+
+        def __init__(self):
+            self.b1 = B1()
+
+    # First level.
+
+    story = A1()
+    state = State()
+    with pytest.raises(m._StepError) as exc_info:
+        r.run(story, state)
+    assert isinstance(exc_info.value, m._StepError)
+    assert str(exc_info.value) == "error in a1s3"
+
+    # Second level.
+
+    story = B1()
+    state = State()
+    with pytest.raises(m._StepError) as exc_info:
+        r.run(story, state)
+    assert isinstance(exc_info.value, m._StepError)
+    assert str(exc_info.value) == "error in b1s1"
+
+    # Third level.
+
+    story = C1()
+    state = State()
+    with pytest.raises(m._StepError) as exc_info:
+        r.run(story, state)
+    assert isinstance(exc_info.value, m._StepError)
+    assert str(exc_info.value) == "error in c1s1"
