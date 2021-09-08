24ed603624f0f49a1e1d025296cc0494f7087095
Artem Malyshev <proofit404@gmail.com>
2021-09-09 00:34:41 +0300

    feat: add argument and variable placeholders #704

--- src/stories/__init__.py
+++ src/stories/__init__.py
@@ -1,8 +1,10 @@
 """Service objects designed with OOP in mind."""
+from _stories.argument import Argument
 from _stories.initiate import initiate
 from _stories.state import State
 from _stories.story import Story
 from _stories.stubs import I
+from _stories.variable import Variable
 
 
-__all__ = ["Story", "State", "I", "initiate"]
+__all__ = ["Story", "State", "I", "initiate", "Argument", "Variable"]
