706f491f184853cf4dc4039d36b2cc245b9011b4
Artem Malyshev <proofit404@gmail.com>
2021-11-26 03:02:29 +0300

    feat: declare variable including validation #606

--- src/_stories/state.py
+++ src/_stories/state.py
@@ -1,6 +1,59 @@
-class State:
-    """Business process state."""
+from _stories.variable import Variable
 
-    def __init__(self, **arguments):
-        for argument, value in arguments.items():
-            object.__setattr__(self, argument, value)
+
+def _setter(state, name, value):
+    object.__setattr__(state, name, value)
+
+
+def _initiator(state, **arguments):
+    for argument, value in arguments.items():
+        _setter(state, argument, value)
+
+
+class _ValidateSetter:
+    def __init__(self, variables):
+        self.variables = variables
+
+    def __get__(self, state, state_class):
+        return _BoundValidateSetter(self.variables, state)
+
+
+class _BoundValidateSetter:
+    def __init__(self, variables, state):
+        self.variables = variables
+        self.state = state
+
+    def __call__(self, name, value):
+        validator = self.variables[name]
+        validated = validator(value)
+        _setter(self.state, name, validated)
+
+
+class _StateType(type):
+    def __new__(cls, class_name, bases, namespace):
+        if bases:
+            variables = {
+                k: v.validate for k, v in namespace.items() if isinstance(v, Variable)
+            }
+            scope = {
+                "__setattr__": _ValidateSetter(variables),
+                "__init__": _initiator,  # pragma: no mutate
+            }
+        else:
+            scope = {
+                "__setattr__": _setter,  # pragma: no mutate
+                "__init__": _initiator,
+            }
+        return type.__new__(cls, class_name, bases, scope)
+
+    def __and__(cls, other):
+        union = {
+            k: Variable(v)
+            for state_class in [cls, other]
+            for k, v in state_class.__setattr__.variables.items()
+        }
+        return type(cls.__name__, (State,), union)
+
+
+class State(metaclass=_StateType):
+    """Business process state."""
--- tests/test_variables.py
+++ tests/test_variables.py
@@ -1,10 +1,216 @@
 """Tests related to story variables."""
+import pytest
+
+from stories import I
+from stories import State
+from stories import Story
 from stories import Variable
+from validators import _is_integer
+from validators import _ValidationError
 
 
-def test_placeholder():
-    """Verify Variable placeholder."""
-    variable = Variable()
-    assert variable.validate is None
-    variable = Variable(lambda: 1)
-    assert variable.validate() == 1
+def test_successful_validation(r, m):
+    """Variable runs validation on state attribute assignment."""
+
+    class A1(Story):
+        I.a1s1
+        I.a1s2
+        I.a1s3
+
+        a1s1 = m._normal_method
+        a1s2 = m._assign_method("a1v1", 1)
+        a1s3 = m._normal_method
+
+    class A1State(State):
+        a1v1 = Variable(_is_integer)
+
+    class B1(Story):
+        I.b1s1
+        I.a1
+        I.b1s2
+
+        b1s1 = m._assign_method("b1v1", 2)
+        b1s2 = m._normal_method
+
+        def __init__(self):
+            self.a1 = A1()
+
+    class B1State(State):
+        b1v1 = Variable(_is_integer)
+
+    class C1(Story):
+        I.c1s1
+        I.b1
+        I.c1s2
+
+        c1s1 = m._assign_method("c1v1", 3)
+        c1s2 = m._normal_method
+
+        def __init__(self):
+            self.b1 = B1()
+
+    class C1State(State):
+        c1v1 = Variable(_is_integer)
+
+    # First level.
+
+    story = A1()
+    state = A1State()
+    r.run(story, state)
+    assert state.a1v1 == 1
+
+    # Second level.
+
+    story = B1()
+    state_class = B1State & A1State
+    state = state_class()
+    r.run(story, state)
+    assert state.b1v1 == 2
+
+    # Third level.
+
+    story = C1()
+    state_class = C1State & B1State & A1State
+    state = state_class()
+    r.run(story, state)
+    assert state.c1v1 == 3
+
+
+def test_failed_validation(r, m):
+    """Validator function should raise an error."""
+
+    class A1(Story):
+        I.a1s1
+        I.a1s2
+        I.a1s3
+
+        a1s1 = m._normal_method
+        a1s2 = m._assign_method("a1v1", "foo")
+        a1s3 = m._normal_method
+
+    class A1State(State):
+        a1v1 = Variable(_is_integer)
+
+    class B1(Story):
+        I.b1s1
+        I.a1
+        I.b1s2
+
+        b1s1 = m._assign_method("b1v1", "bar")
+        b1s2 = m._normal_method
+
+        def __init__(self):
+            self.a1 = A1()
+
+    class B1State(State):
+        b1v1 = Variable(_is_integer)
+
+    class C1(Story):
+        I.c1s1
+        I.b1
+        I.c1s2
+
+        c1s1 = m._assign_method("c1v1", "baz")
+        c1s2 = m._normal_method
+
+        def __init__(self):
+            self.b1 = B1()
+
+    class C1State(State):
+        c1v1 = Variable(_is_integer)
+
+    # First level.
+
+    story = A1()
+    state = A1State()
+    with pytest.raises(_ValidationError) as exc_info:
+        r.run(story, state)
+    assert not hasattr(state, "a1v1")
+    assert exc_info.value.args == ("foo",)
+
+    # Second level.
+
+    story = B1()
+    state_class = B1State & A1State
+    state = state_class()
+    with pytest.raises(_ValidationError) as exc_info:
+        r.run(story, state)
+    assert not hasattr(state, "b1v1")
+    assert exc_info.value.args == ("bar",)
+
+    # Third level.
+
+    story = C1()
+    state_class = C1State & B1State & A1State
+    state = state_class()
+    with pytest.raises(_ValidationError) as exc_info:
+        r.run(story, state)
+    assert not hasattr(state, "c1v1")
+    assert exc_info.value.args == ("baz",)
+
+
+def test_value_normalization(r, m):
+    """Validator function should return normalized value."""
+
+    class A1(Story):
+        I.a1s1
+        I.a1s2
+        I.a1s3
+
+        a1s1 = m._normal_method
+        a1s2 = m._assign_method("a1v1", "1")
+        a1s3 = m._normal_method
+
+    class A1State(State):
+        a1v1 = Variable(_is_integer)
+
+    class B1(Story):
+        I.b1s1
+        I.a1
+        I.b1s2
+
+        b1s1 = m._assign_method("b1v1", "2")
+        b1s2 = m._normal_method
+
+        def __init__(self):
+            self.a1 = A1()
+
+    class B1State(State):
+        b1v1 = Variable(_is_integer)
+
+    class C1(Story):
+        I.c1s1
+        I.b1
+        I.c1s2
+
+        c1s1 = m._assign_method("c1v1", "3")
+        c1s2 = m._normal_method
+
+        def __init__(self):
+            self.b1 = B1()
+
+    class C1State(State):
+        c1v1 = Variable(_is_integer)
+
+    # First level.
+
+    story = A1()
+    state = A1State()
+    r.run(story, state)
+    assert state.a1v1 == 1
+
+    # Second level.
+
+    story = B1()
+    state_class = B1State & A1State
+    state = state_class()
+    r.run(story, state)
+    assert state.b1v1 == 2
+
+    # Third level.
+
+    story = C1()
+    state_class = C1State & B1State & A1State
+    state = state_class()
+    r.run(story, state)
+    assert state.c1v1 == 3
