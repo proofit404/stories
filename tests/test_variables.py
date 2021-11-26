5b27e46666ea5e064b8640d188ef07c256cd88ee
Artem Malyshev <proofit404@gmail.com>
2021-11-27 02:12:50 +0300

    feat: deny to assign undeclared variable #680

--- tests/test_variables.py
+++ tests/test_variables.py
@@ -5,6 +5,7 @@ from stories import I
 from stories import State
 from stories import Story
 from stories import Variable
+from stories.exceptions import StateError
 from validators import _is_integer
 from validators import _ValidationError
 
@@ -75,6 +76,97 @@ def test_no_validation(r, m, value):
     state = state_class()
     r.run(story, state)
     assert state.c1v1 == value
+
+
+def test_unknown_variable(r, m):
+    """Deny attribute assignment not defined by Variable."""
+
+    class A1(Story):
+        I.a1s1
+        I.a1s2
+        I.a1s3
+
+        a1s1 = m._normal_method
+        a1s2 = m._assign_method("a1v2", 1)
+        a1s3 = m._normal_method
+
+    class A1State(State):
+        a1v1 = Variable()
+
+    class B1(Story):
+        I.b1s1
+        I.a1
+        I.b1s2
+
+        b1s1 = m._assign_method("b1v2", 2)
+        b1s2 = m._normal_method
+
+        def __init__(self):
+            self.a1 = A1()
+
+    class B1State(State):
+        b1v1 = Variable()
+
+    class C1(Story):
+        I.c1s1
+        I.b1
+        I.c1s2
+
+        c1s1 = m._assign_method("c1v2", 3)
+        c1s2 = m._normal_method
+
+        def __init__(self):
+            self.b1 = B1()
+
+    class C1State(State):
+        c1v1 = Variable()
+
+    # First level.
+
+    story = A1()
+    state = A1State()
+    with pytest.raises(StateError) as exc_info:
+        r.run(story, state)
+    assert (
+        str(exc_info.value)
+        == """
+Unknown variable assignment: a1v2
+
+A1State
+    """.strip()
+    )
+
+    # Second level.
+
+    story = B1()
+    state_class = B1State & A1State
+    state = state_class()
+    with pytest.raises(StateError) as exc_info:
+        r.run(story, state)
+    assert (
+        str(exc_info.value)
+        == """
+Unknown variable assignment: b1v2
+
+B1State & A1State
+    """.strip()
+    )
+
+    # Third level.
+
+    story = C1()
+    state_class = C1State & B1State & A1State
+    state = state_class()
+    with pytest.raises(StateError) as exc_info:
+        r.run(story, state)
+    assert (
+        str(exc_info.value)
+        == """
+Unknown variable assignment: c1v2
+
+C1State & B1State & A1State
+    """.strip()
+    )
 
 
 def test_successful_validation(r, m):
