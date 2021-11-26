d4146097ff53e786a05d559f0322be74dcef5abf
Artem Malyshev <proofit404@gmail.com>
2021-11-26 23:34:50 +0300

    feat: declare variable without validation #591

--- tests/test_variables.py
+++ tests/test_variables.py
@@ -9,6 +9,74 @@ from validators import _is_integer
 from validators import _ValidationError
 
 
+@pytest.mark.parametrize("value", [1, "x", None])
+def test_no_validation(r, m, value):
+    """Variable allow state attribute assignment to any value."""
+
+    class A1(Story):
+        I.a1s1
+        I.a1s2
+        I.a1s3
+
+        a1s1 = m._normal_method
+        a1s2 = m._assign_method("a1v1", value)
+        a1s3 = m._normal_method
+
+    class A1State(State):
+        a1v1 = Variable()
+
+    class B1(Story):
+        I.b1s1
+        I.a1
+        I.b1s2
+
+        b1s1 = m._assign_method("b1v1", value)
+        b1s2 = m._normal_method
+
+        def __init__(self):
+            self.a1 = A1()
+
+    class B1State(State):
+        b1v1 = Variable()
+
+    class C1(Story):
+        I.c1s1
+        I.b1
+        I.c1s2
+
+        c1s1 = m._assign_method("c1v1", value)
+        c1s2 = m._normal_method
+
+        def __init__(self):
+            self.b1 = B1()
+
+    class C1State(State):
+        c1v1 = Variable()
+
+    # First level.
+
+    story = A1()
+    state = A1State()
+    r.run(story, state)
+    assert state.a1v1 == value
+
+    # Second level.
+
+    story = B1()
+    state_class = B1State & A1State
+    state = state_class()
+    r.run(story, state)
+    assert state.b1v1 == value
+
+    # Third level.
+
+    story = C1()
+    state_class = C1State & B1State & A1State
+    state = state_class()
+    r.run(story, state)
+    assert state.c1v1 == value
+
+
 def test_successful_validation(r, m):
     """Variable runs validation on state attribute assignment."""
 
