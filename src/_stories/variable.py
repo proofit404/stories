ef5c574d42314dc7272948b5910d686cce2b59f9
Artem Malyshev <proofit404@gmail.com>
2021-11-26 23:34:50 +0300

    feat: declare variable without validation #591

--- src/_stories/variable.py
+++ src/_stories/variable.py
@@ -2,4 +2,4 @@ class Variable:
     """Variable definition in state class."""
 
     def __init__(self, validate=None):
-        self.validate = validate
+        self.validate = validate or (lambda value: value)
