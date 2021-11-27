60acffb528430d7e024b33d5133fe9d6b00c0910
Artem Malyshev <proofit404@gmail.com>
2021-11-27 22:01:09 +0300

    feat: story steps could assign state variable #693

--- tests/test_stories.py
+++ tests/test_stories.py
@@ -115,6 +115,116 @@ def test_execute_steps(r, m):
     assert state.calls == ["c1s1", "b1s1", "a1s1", "a1s2", "a1s3", "b1s2", "c1s2"]
 
 
+def test_assign_state_attribute(r, m):
+    """Variables assigned by previous steps are available in current step."""
+
+    class A1(Story):
+        I.a1s1
+        I.a1s2
+
+        a1s1 = m._assign_method("a1v1", 1)
+        a1s2 = m._assert_method("a1v1", 1)
+
+    class B1(Story):
+        I.b1s1
+        I.a1
+        I.b1s2
+
+        b1s1 = m._assign_method("b1v1", 2)
+        b1s2 = m._assert_method("b1v1", 2)
+
+        def __init__(self):
+            self.a1 = A1()
+
+    class C1(Story):
+        I.c1s1
+        I.b1
+        I.c1s2
+
+        c1s1 = m._assign_method("c1v1", 3)
+        c1s2 = m._assert_method("c1v1", 3)
+
+        def __init__(self):
+            self.b1 = B1()
+
+    # First level.
+
+    story = A1()
+    state = State()
+    r.run(story, state)
+
+    # Second level.
+
+    story = B1()
+    state = State()
+    r.run(story, state)
+
+    # Third level.
+
+    story = C1()
+    state = State()
+    r.run(story, state)
+
+
+def test_access_state_attributes(r, m):
+    """State changed made by story should be available to caller code."""
+
+    class A1(Story):
+        I.a1s1
+        I.a1s2
+        I.a1s3
+
+        a1s1 = m._normal_method
+        a1s2 = m._assign_method("a1v1", 1)
+        a1s3 = m._normal_method
+
+    class B1(Story):
+        I.b1s1
+        I.a1
+        I.b1s2
+
+        b1s1 = m._assign_method("b1v1", 2)
+        b1s2 = m._normal_method
+
+        def __init__(self):
+            self.a1 = A1()
+
+    class C1(Story):
+        I.c1s1
+        I.b1
+        I.c1s2
+
+        c1s1 = m._assign_method("c1v1", 3)
+        c1s2 = m._normal_method
+
+        def __init__(self):
+            self.b1 = B1()
+
+    # First level.
+
+    story = A1()
+    state = State()
+    r.run(story, state)
+    assert state.a1v1 == 1
+
+    # Second level.
+
+    story = B1()
+    state = State()
+    r.run(story, state)
+    assert state.a1v1 == 1
+    assert state.b1v1 == 2
+
+    # Third level.
+
+    story = C1()
+    state = State()
+    r.run(story, state)
+    assert state.a1v1 == 1
+    assert state.b1v1 == 2
+    assert state.c1v1 == 3
+
+
 def test_propagate_exceptions(r, m):
     """Exceptions raised by story steps should be propagated to the caller."""
 
