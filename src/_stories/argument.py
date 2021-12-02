0b5b1738f0d318400aaf8230de037550bbfb9295
Artem Malyshev <proofit404@gmail.com>
2021-12-02 05:22:04 +0300

    feat: declare argument including validation #605

--- src/_stories/argument.py
+++ src/_stories/argument.py
@@ -1,5 +1,5 @@
 class Argument:
     """Argument definition in state class."""
 
-    def __init__(self):
-        self.validate = lambda value: value
+    def __init__(self, validate=None):
+        self.validate = validate or (lambda value: value)
