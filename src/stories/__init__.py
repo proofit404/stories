e3b48fb17fd50e6817ca2a5045fa53bf7998d601
Josiah Kaviani <proofit404@gmail.com>
2023-04-06 01:05:41 +0300

    feat: implement story function #723

--- src/stories/__init__.py
+++ src/stories/__init__.py
@@ -1,1 +1,5 @@
 """Service objects designed with OOP in mind."""
+from stories.story import story
+
+
+__all__ = ["story"]
---
+++ tests/test_story.py
@@ -0,0 +1,76 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from dataclasses import field
+
+from stories import story
+
+
+def test_execute_steps() -> None:
+    @story
+    def a1(it: A1Steps, state: State) -> None:
+        it.a1s1(state)
+        it.a1s2(state)
+        it.a1s3(state)
+
+    @story
+    def b1(it: B1Steps, state: State) -> None:
+        it.b1s1(state)
+        it.a1(state)
+        it.b1s2(state)
+
+    @story
+    def c1(it: C1Steps, state: State) -> None:
+        it.c1s1(state)
+        it.b1(state)
+        it.c1s2(state)
+
+    # First level.
+
+    state = State()
+    a1(A1Steps(), state)
+    assert state.calls == ["a1s1", "a1s2", "a1s3"]
+
+    # Second level.
+
+    state = State()
+    b1(B1Steps(), state)
+    assert state.calls == ["b1s1", "a1s1", "a1s2", "a1s3", "b1s2"]
+
+    # Third level.
+
+    state = State()
+    c1(C1Steps(), state)
+    assert state.calls == ["c1s1", "b1s1", "a1s1", "a1s2", "a1s3", "b1s2", "c1s2"]
+
+
+class A1Steps:
+    def a1s1(self, state: State) -> None:
+        state.calls.append("a1s1")
+
+    def a1s2(self, state: State) -> None:
+        state.calls.append("a1s2")
+
+    def a1s3(self, state: State) -> None:
+        state.calls.append("a1s3")
+
+
+class B1Steps:
+    def b1s1(self, state: State) -> None:
+        state.calls.append("b1s1")
+
+    def b1s2(self, state: State) -> None:
+        state.calls.append("b1s2")
+
+
+class C1Steps:
+    def c1s1(self, state: State) -> None:
+        state.calls.append("c1s1")
+
+    def c1s2(self, state: State) -> None:
+        state.calls.append("c1s2")
+
+
+@dataclass
+class State:
+    calls: list[str] = field(default_factory=list)
