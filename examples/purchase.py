d4d93d79dae0859d7e5c10adc573565471fef526
Josiah Kaviani <proofit404@gmail.com>
2023-04-08 03:36:52 +0300

    wtf

--- examples/purchase.py
+++ examples/purchase.py
@@ -4,6 +4,11 @@ from collections.abc import Callable
 from dataclasses import dataclass
 from types import SimpleNamespace as State
 
+from app.entities import Customer
+from app.entities import CustomerId
+from app.entities import Order
+from app.entities import OrderId
+from app.entities import Payment
 from app.repositories import create_payment
 from app.repositories import load_customer
 from app.repositories import load_order
@@ -11,19 +16,14 @@ from app.repositories import load_order
 from stories import story
 
 
-@story
-def purchase(it: Steps, state: State) -> None:
-    it.find_order(state)
-    it.find_customer(state)
-    it.check_balance(state)
-    it.persist_payment(state)
-
-
 @dataclass
-class Steps:
-    load_order: Callable
-    load_customer: Callable
-    create_payment: Callable
+class Purchase:
+    @story
+    def make(self, state: State) -> None:
+        self.find_order(state)
+        self.find_customer(state)
+        self.check_balance(state)
+        self.persist_payment(state)
 
     def find_order(self, state: State) -> None:
         state.order = self.load_order(state.order_id)
@@ -36,12 +36,14 @@ class Steps:
             raise Exception
 
     def persist_payment(self, state: State) -> None:
-        state.payment = self.create_payment(
-            order_id=state.order_id, customer_id=state.customer_id
-        )
+        state.payment = self.create_payment(state.order_id, state.customer_id)
+
+    load_order: Callable[[OrderId], Order]
+    load_customer: Callable[[CustomerId], Customer]
+    create_payment: Callable[[OrderId, CustomerId], Payment]
 
 
-steps = Steps(
+purchase = Purchase(
     load_order=load_order, load_customer=load_customer, create_payment=create_payment
 )
 
@@ -49,7 +51,7 @@ steps = Steps(
 state = State(order_id=1, customer_id=1)
 
 
-purchase(steps, state)
+purchase.make(state)
 
 
 print(state.payment)
